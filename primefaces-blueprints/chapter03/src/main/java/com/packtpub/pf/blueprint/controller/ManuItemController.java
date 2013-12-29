5fd44f2a1ac4351eb7779a0c58ee36292758b5f2
Ramkumar Pillai <psramkumar@gmail.com>
2013-12-29 10:20:44 +0530

    Basic Layout and pfExt

--- primefaces-blueprints/chapter03/src/main/java/com/packtpub/pf/blueprint/controller/ManuItemController.java
+++ primefaces-blueprints/chapter03/src/main/java/com/packtpub/pf/blueprint/controller/ManuItemController.java
@@ -1,5 +1,11 @@
 package com.packtpub.pf.blueprint.controller;
 
+import org.apache.log4j.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 /**
  * Created with IntelliJ IDEA.
  * User: psramkumar
@@ -7,5 +13,13 @@ package com.packtpub.pf.blueprint.controller;
  * Time: 12:39 PM
  * To change this template use File | Settings | File Templates.
  */
+@ManagedBean
+@RequestScoped
 public class ManuItemController {
+    private static final Logger _log = Logger.getLogger(ManuItemController.class);
+
+    @PostConstruct
+    public void init() {
+        _log.info("Initilized now here : " + System.currentTimeMillis());
+    }
 }
