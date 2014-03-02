0e111073460362ba949323ac2b0e5c71d359f5d0
sudheerj <sudheer.jonna@gmail.com>
2014-03-02 20:28:55 +0530

    DealerInfo updated

--- primefaces-blueprints/chapter04/src/main/java/com/packt/pfblueprints/model/Dealer.java
+++ primefaces-blueprints/chapter04/src/main/java/com/packt/pfblueprints/model/Dealer.java
@@ -16,7 +16,48 @@ public class Dealer implements Serializable{
 	private String branch;
 	private String status;
 	
-	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDealernumber() {
+		return dealernumber;
+	}
+	public void setDealernumber(String dealernumber) {
+		this.dealernumber = dealernumber;
+	}
+	public String getAdvisorname() {
+		return advisorname;
+	}
+	public void setAdvisorname(String advisorname) {
+		this.advisorname = advisorname;
+	}
+	public String getAdvisornumber() {
+		return advisornumber;
+	}
+	public void setAdvisornumber(String advisornumber) {
+		this.advisornumber = advisornumber;
+	}
+	public String getDor() {
+		return dor;
+	}
+	public void setDor(String dor) {
+		this.dor = dor;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	
 
 }
