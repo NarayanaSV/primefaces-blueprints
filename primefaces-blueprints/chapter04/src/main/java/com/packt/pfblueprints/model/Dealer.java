9b43a897766fbe27a53e5b064b9630e22281b974
sudheerj <sudheer.jonna@gmail.com>
2014-03-02 20:08:32 +0530

    Dealer POJO committed

---
+++ primefaces-blueprints/chapter04/src/main/java/com/packt/pfblueprints/model/Dealer.java
@@ -0,0 +1,22 @@
+package com.packt.pfblueprints.model;
+
+import java.io.Serializable;
+
+public class Dealer implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String dealernumber;
+	private String advisorname;
+	private String advisornumber;
+	private String dor;
+	private String branch;
+	private String status;
+	
+	
+	
+
+}
